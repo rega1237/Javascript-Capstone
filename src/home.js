f253edd7fab52f692c06675c5339ab3789ce0ce3
Bassem <bassem@MacBook-Pro-de-Bassem.local>
2022-05-17 22:42:01 -0300

    Create showLikes function to show the likes count on home page

--- src/home.js
+++ src/home.js
@@ -1,7 +1,9 @@
 /* eslint-disable no-await-in-loop */
 
-const container = document.querySelector('.items-container');
-const pokemonList = 'https://pokeapi.co/api/v2/pokemon/?limit=9&offset=0';
+const container = document.querySelector(".items-container");
+const pokemonList = "https://pokeapi.co/api/v2/pokemon/?limit=9&offset=0";
+const likeApi =
+  "https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/dXDIQAnIOHUjELoXSV9S/likes";
 let pokemons = [];
 
 const getData = async (Api) => {
@@ -22,15 +24,34 @@ const pokemnData = async () => {
   return pokemons;
 };
 
-const pokemonShow = async () => {
+const likesShow = async () => {
+  const likes = await getData(likeApi);
   const pokemons = await pokemnData();
-  container.innerHTML = '';
+  for (let i = 0; i < likes.length; i += 1) {
+    for (let x = 0; x < pokemons.length; x += 1) {
+      if (likes[i].item_id == pokemons[x].id) {
+        pokemons[x].likes = likes[i].likes;
+      }
+    }
+  }
+  return pokemons;
+}
+
+const pokemonShow = async () => {
+  const pokemons = await likesShow();
+  container.innerHTML = "";
   for (let i = 0; i < pokemons.length; i += 1) {
     container.innerHTML += `<article class="pokemon-card">
-        <img src="${pokemons[i].img}" alt="${pokemons[i].name}" class="pokemon-img"/>
+        <img src="${pokemons[i].img}" alt="${
+      pokemons[i].name
+    }" class="pokemon-img"/>
         <h2 class="pokemon-name">${pokemons[i].name.toUpperCase()}</h2>
-        <a href="#" class="comment" id="${pokemons[i].id}">Comment</a></article>`;
+        <p class="likes-counter">${pokemons[i].likes} likes</p>
+        <a href="#" class="comment" id="${
+          pokemons[i].id
+        }">Comment</a></article>`;
   }
 };
 
 export default pokemonShow;
+// dXDIQAnIOHUjELoXSV9S;
